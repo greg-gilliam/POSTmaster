0bbf31ab0f6666e0561a3799857079c273513749
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-05 17:05:24 -0700

    container setup

---
+++ src/Containers/POSTmasterContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class POSTmasterContainer extends Component {
+  state = {
+    loading: true,
+    urlInput: '',
+    method: '',
+    history: [],
+    response: '',
+    jsonInput: '',
+  };
+  render() {
+    return <div></div>;
+  }
+}
+
+export default POSTmasterContainer;
