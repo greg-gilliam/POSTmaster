190a6f38e330384863ef6e4eea90e3d5dcd94af3
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-11-07 19:03:59 -0800

    added super basic css & built out container

--- src/Containers/POSTmasterContainer.jsx
+++ src/Containers/POSTmasterContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import style from './styles/POSTmasterContainer.css';
 
 class POSTmasterContainer extends Component {
   state = {
@@ -9,8 +10,43 @@ class POSTmasterContainer extends Component {
     response: '',
     jsonInput: '',
   };
+
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [this.name]: target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { urlInput, method, jsonInput, history } = this.state;
+    this.setState({
+      loading: true,
+      method,
+      history: [...history, { url: urlInput, method: method }],
+    });
+    const response = await fetchRequest(method, urlInput, jsonInput);
+    this.setState({ response, loading: false });
+  };
   render() {
-    return <div></div>;
+    const { urlInput, method, jsonInput, history, response } = this.state;
+    return (
+      <section className={style.container}>
+        <Header className={style.header} />
+        <Form
+          className={style.form}
+          urlInput={urlInput}
+          method={method}
+          jsonInput={jsonInput}
+          onInput={this.handleChange}
+          onSubmit={this.handleSubmit}
+        />
+        <History className={style.history} history={history} />
+        <Display className={style.display} response={response} />
+      </section>
+    );
   }
 }
 
